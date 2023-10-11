f4efb3596544af4656588e3dee514c3bfe75f165
Joakim <joakim.nystrom@amaceit.se>
2023-10-11 12:57:23 +0200

    fixes lint errors

--- src/app/components/Menu/HeaderMenu.tsx
+++ src/app/components/Menu/HeaderMenu.tsx
@@ -1,10 +1,14 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-const HeaderMenu = ({ blok }) => (
+import {
+  ConfigStoryblok,
+  MenuLinkStoryblok,
+} from "../../../../component-types-sb";
+const HeaderMenu = ({ blok }: { blok: ConfigStoryblok }) => (
   <div
     className="hidden md:flex items-center justify-end md:flex-1 lg:w-0 space-x-10"
     {...storyblokEditable({ blok })}
   >
-    {blok.links.map((nestedBlok) => (
+    {blok.links.map((nestedBlok: MenuLinkStoryblok) => (
       <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
     ))}
   </div>
--- src/app/components/Menu/MenuLink.tsx
+++ src/app/components/Menu/MenuLink.tsx
@@ -1,13 +1,17 @@
 import { storyblokEditable } from "@storyblok/react";
 import Link from "next/link";
-const MenuLink = ({ blok }) => (
+import { MenuLinkStoryblok } from "../../../../component-types-sb";
+const MenuLink = ({ blok }: MenuLinkStoryblok) => (
   <>
     <Link href={blok.link.cached_url} {...storyblokEditable(blok)}>
       <span className="text-base font-medium text-gray-500 hover:text-gray-900">
         {blok.name}
       </span>
     </Link>
-    {blok?.children && blok.children.map((child) => <p>{child.name}</p>)}
+    {blok?.children &&
+      blok.children.map((child: MenuLinkStoryblok) => (
+        <p key={child.id}>{child.name}</p>
+      ))}
   </>
 );
 export default MenuLink;
