808c6b771a2b550700f0f81eff4ee80b617bd440
Joakim <joakim.nystrom@amaceit.se>
2023-10-12 10:33:27 +0200

    Adds support for sub-items in menu

--- src/app/components/Menu/MenuLink.tsx
+++ src/app/components/Menu/MenuLink.tsx
@@ -1,17 +1,23 @@
-import { storyblokEditable } from "@storyblok/react";
+import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 import Link from "next/link";
 import { MenuLinkStoryblok } from "../../../../component-types-sb";
-const MenuLink = ({ blok }: MenuLinkStoryblok) => (
-  <>
-    <Link href={blok.link.cached_url} {...storyblokEditable(blok)}>
-      <span className="text-base font-medium text-gray-500 hover:text-gray-900">
-        {blok.name}
-      </span>
-    </Link>
-    {blok?.children &&
-      blok.children.map((child: MenuLinkStoryblok) => (
-        <p key={child.id}>{child.name}</p>
-      ))}
-  </>
-);
+const MenuLink = ({ blok }: { blok: MenuLinkStoryblok }) => {
+  return (
+    <>
+      <Link href={blok?.link?.cached_url} {...storyblokEditable(blok)}>
+        <span className="text-base font-medium text-gray-500 hover:text-gray-900 ">
+          {blok.title}
+        </span>
+      </Link>
+      {typeof blok?.subItems !== "string" &&
+        blok.subItems?.map((child: MenuLinkStoryblok) => (
+          <>
+            <StoryblokComponent
+              blok={{ ...child, title: child.title + " (child)" }}
+            />
+          </>
+        ))}
+    </>
+  );
+};
 export default MenuLink;
