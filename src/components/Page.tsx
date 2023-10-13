61fcc39988414b972a9b9e85c9b1619c9cc12708
Joakim <joakim.nystrom@amaceit.se>
2023-10-13 13:37:11 +0200

    Removes section wrapper for page.

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -6,10 +6,7 @@ const Page = ({ blok }: PageStoryblok) => {
   return (
     <main className="text-center mt-4" {...storyblokEditable(blok)}>
       {blok.body?.map((nestedBlok: { _uid: string }) => (
-        <SectionWrapper color="primary" title="Title">
-          <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-          <h1>test</h1>
-        </SectionWrapper>
+        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
     </main>
   );
