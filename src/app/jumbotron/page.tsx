9c0aadf3e731fff054b94dce10eff53597a2ce3c
Joakim <joakim.nystrom@amaceit.se>
2023-10-16 16:03:34 +0200

    Demo page + assets

--- src/app/jumbotron/page.tsx
+++ src/app/jumbotron/page.tsx
@@ -1,9 +1,30 @@
+import { Button } from "@/components/Button";
 import { Jumbotron } from "@/components/Jumbotron";
+import Image from "next/image";
 
 const JumbotronPage = () => {
   return (
-    <div>
-      <Jumbotron variant="background" backgroundColor="gray" />
+    <div className="">
+      <Jumbotron
+        title="Vi vill göra upphandling till en förvånande enkel process"
+        imageUrl={`/hero-images/shape_40.png`}
+      >
+        <p className="mb-2">
+          Har ni börjat titta på uppdhandlingar och funderat på att bygga det
+          internt pga krånglet med kravställning?
+        </p>
+      </Jumbotron>
+
+      <Jumbotron
+        variant="content-right"
+        title="Vi vill göra upphandling till en förvånande enkel process"
+        imageUrl={`/hero-images/shape_50.png`}
+      >
+        <p className="mb-2">
+          Har ni börjat titta på uppdhandlingar och funderat på att bygga det
+          internt pga krånglet med kravställning?
+        </p>
+      </Jumbotron>
     </div>
   );
 };
