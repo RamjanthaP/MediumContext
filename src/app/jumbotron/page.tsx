153737098ebde161df3db836d6f01fc93cd53675
Joakim <joakim.nystrom@amaceit.se>
2023-10-16 06:42:32 +0200

    Adds mock page

---
+++ src/app/jumbotron/page.tsx
@@ -0,0 +1,11 @@
+import { Jumbotron } from "@/components/Jumbotron";
+
+const JumbotronPage = () => {
+  return (
+    <div>
+      <Jumbotron variant="background" backgroundColor="gray" />
+    </div>
+  );
+};
+
+export default JumbotronPage;
