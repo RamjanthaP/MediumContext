16abee57f92f9bb87875e1b21ce0d8b16675d81e
Tuliany <tuliany.grande@amaceit.se>
2023-10-24 12:17:39 +0200

    logo responsive to dark/light mode

--- src/components/Menu/AppHeader.tsx
+++ src/components/Menu/AppHeader.tsx
@@ -5,25 +5,21 @@ import Link from "next/link";
 import { MenuStoryblok, MenuLinkStoryblok } from "../../../component-types-sb";
 import Image from "next/image";
 import Button from "../Button/Button";
+import Amaceit from "../Logo/Amaceit";
 
 
 const Menu = ({ blok }: { blok: MenuStoryblok }) => {
   console.log(blok?.content.header_menu)
   return (
     <div
-      className="relative bg-white border-b-2 border-gray-100"
+      className="relative bg-default border-b-2 border-gray-100"
       {...storyblokEditable(blok)}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+      <div className="container mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center  py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href="/">
-              <Image
-                src="/logos/amaceit.png"
-                alt=""
-                width={150}
-                height={53}
-              />
+              <Amaceit />
             </Link>
           </div>
           {/* TODO: exchange the title from "tjänster" to "kontakta oss" */}
