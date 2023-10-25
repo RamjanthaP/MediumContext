1c9210cba52c2ab2dbef236b96235dee8518f6ca
Joakim <joakim.nystrom@amaceit.se>
2023-10-25 14:00:58 +0200

    Adds sizing to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,42 +1,54 @@
-import React from 'react';
+import React from "react";
+
+export type ButtonSizes = keyof typeof sizeClass;
+export type ButtonColors = keyof typeof colorVariants;
 
 type ButtonProps = {
-  variant?: 'green' | 'white' | 'black';
+  variant?: ButtonColors;
   transparent?: boolean;
   children: React.ReactNode;
-  [x: string]: any; 
-}
+  size?: ButtonSizes;
+  [x: string]: any;
+};
 
 const colorVariants = {
   green: {
-    default: 'btn-green',
-    transparent: 'btn-green-transparent',
+    default: "btn-green",
+    transparent: "btn-green-transparent",
   },
   white: {
-    default: 'btn-white',
-    transparent: 'btn-white-transparent',
+    default: "btn-white",
+    transparent: "btn-white-transparent",
   },
   black: {
-    default: 'btn-black',
-    transparent: 'btn-black-transparent',
+    default: "btn-black",
+    transparent: "btn-black-transparent",
   },
 };
 
+const sizeClass = {
+  small: "px-4 py-2",
+  medium: "px-6 py-3",
+  large: "px-7 py-3",
+};
+
 const Button: React.FC<ButtonProps> = ({
-  variant = 'green',
+  variant = "green",
   transparent = false,
+  size = "medium",
   children,
   ...props
 }) => {
-  const baseClasses = "mx-2 p-4 rounded-full border-2 uppercase whitespace-pre ";
-  const colorClass = colorVariants[variant][transparent ? 'transparent' : 'default'];
+  const baseClasses = `mx-2 rounded-full border-2 uppercase whitespace-pre`;
+  const colorClass =
+    colorVariants[variant][transparent ? "transparent" : "default"];
+  const sizeClasses = sizeClass[size];
 
   return (
-    <button className={`${baseClasses} ${colorClass}`} {...props}>
+    <button className={`${baseClasses} ${colorClass} ${sizeClass}`} {...props}>
       {children}
     </button>
   );
 };
 
 export default Button;
-
