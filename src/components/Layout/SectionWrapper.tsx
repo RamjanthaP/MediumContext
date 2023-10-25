fafc3d598a36d53536cb93b55632e6c1fd287128
Joakim <joakim.nystrom@amaceit.se>
2023-10-25 21:44:13 +0200

    Adds improved components for layout.

---
+++ src/components/Layout/SectionWrapper.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container } from "./Container";
+import { BaseProps } from "@/components/PropsHelpers";
+
+interface SectionWrapperProps extends BaseProps {
+  color?: "default" | "inverted" | "discrete";
+  title?: string;
+  titleElement?: "h1" | "h2" | "h3" | "span";
+}
+
+const SectionWrapper: React.FC<SectionWrapperProps> = ({
+  color = "default",
+  title,
+  titleElement,
+  children,
+  className = "",
+}) => {
+  const TitleElement = titleElement || "div";
+  return (
+    <div className={`theme-block-${color} ${className}`}>
+      <Container element="section" className="lg:py-16">
+        <div>
+          <TitleElement className="text-2xl font-bold mb-4">
+            {title}
+          </TitleElement>
+          <div className="flex">{children}</div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default SectionWrapper;
