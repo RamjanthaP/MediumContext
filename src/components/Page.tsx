8aad334de1ecbb5468cbba708db638e240b58d77
Joakim <joakim.nystrom@amaceit.se>
2023-10-26 13:54:27 +0200

    removes text-center class

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,10 +1,10 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 import { PageStoryblok } from "../../component-types-sb";
 
-// TODO: Figure out if we are using this.
+// Used as entry point for all pages in Storyblok
 const Page = ({ blok }: PageStoryblok) => {
   return (
-    <main className="text-center mt-4" {...storyblokEditable(blok)}>
+    <main className="mt-4" {...storyblokEditable(blok)}>
       {blok.body?.map((nestedBlok: { _uid: string }) => (
         <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
