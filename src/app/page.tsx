e36438433f15ab876c3fca8315221dcab4c4f1c4
Joakim <joakim.nystrom@amaceit.se>
2023-10-26 15:43:11 +0200

    Removes hard coded hero and register WIP-hero as SB-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { getStoryblokPage } from "@/services/getStoryBlokPage";
 import Hero from "../components/Hero/Hero";
 import { Container } from "@/components/Layout/Container";
 import { SectionServices } from "../components/Section/Services";
+import Placholder from "@/components/DevUtils/Placeholder";
 
 export default async function Home() {
   const { props } = await getStoryblokPage();
@@ -12,13 +13,14 @@ export default async function Home() {
       <Container className="bg-discrete">
         <h1 className="text-3xl font-medium">{props.story.name}</h1>
       </Container>
-      <Hero
+
+      {/* <Hero
         headLine={props.story.content.body[1].headline}
         imageUrl="/assets/Hero.png"
         primaryButtonTitle="Transparent"
         secondaryButtonTitle="Green"
-      />
-      <SectionServices />
+      /> */}
+      {/* <SectionServices /> */}
       <StoryblokStory story={props.story} />
     </div>
   );
