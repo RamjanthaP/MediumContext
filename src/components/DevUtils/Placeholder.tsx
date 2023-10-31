5630e1444889f3a0a4fe922851b6951eaa2ef430
Joakim <joakim.nystrom@amaceit.se>
2023-10-31 17:19:40 +0100

    Updates Placeholder to generic fallback

--- src/components/DevUtils/Placeholder.tsx
+++ src/components/DevUtils/Placeholder.tsx
@@ -1,30 +1,34 @@
 import { BaseProps } from "@/types/props";
 
-const Placholder = (
-  props: Partial<BaseProps> & {
-    name: string;
-    data?: { title?: string; headline?: string } & any;
-  }
-) => {
+interface FallbackBlokProps {
+  title?: string;
+  name?: string;
+  headline?: string;
+  _uid: string;
+  component: string;
+}
+const Placholder = (props: { blok: FallbackBlokProps }) => {
+  const componentName = props.blok.component;
+  const { blok } = props;
+  const { title, name, headline } = blok;
   return (
     <div className="border-primary-500 border-dashed border p-4 m-4 relative">
       <div className="bg-primary-500 text-white px-2 py-0 text-sm inline-block absolute top-0 left-0">
-        Placeholder {props.name}
+        Placeholder {componentName}
       </div>
       <h1 className="text-lg mt-4 text-primary-400">
-        {props.data?.title || props.data?.headline || "utan namn"}
+        {title || headline || name || "utan namn"}
       </h1>
       <div>
         <details>
-          <summary>Data (click to expand)</summary>
-          {!props.children && props.data && (
-            <pre className="whitespace-pre-wrap p-8 text-sm">
-              {JSON.stringify(props.data, null, 2)}
-            </pre>
-          )}
-        </details>
+          <summary className="cursor-pointer hover:text-primary-200">
+            Data (click to expand)
+          </summary>
 
-        {props.children}
+          <pre className="whitespace-pre-wrap p-8 text-sm">
+            {JSON.stringify(blok, null, 2)}
+          </pre>
+        </details>
       </div>
     </div>
   );
