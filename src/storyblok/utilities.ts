d904fc65bb801f8e96b66ac2abd5d8da1476f858
Joakim <joakim.nystrom@amaceit.se>
2023-10-31 12:53:13 +0100

    Adds comment to Storyblok util

--- src/storyblok/utilities.ts
+++ src/storyblok/utilities.ts
@@ -1,3 +1,5 @@
+// Needed in order to use the SVGs from Storyblok in the project
+// Storyblok don't provide CORS headers for their SVGs
 export const getStoryblokCorsUrl = (url: string): string => {
   return url.replace("//a.storyblok.com", "//a2.storyblok.com");
 };
