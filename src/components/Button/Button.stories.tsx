cd45174de26c77c5ac8aed0d10a0c644d0b46c42
Joakim <joakim.nystrom@amaceit.se>
2023-11-03 14:36:34 +0100

    Adds support for Figma preview in Storybook

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,10 +1,10 @@
-import Button from "./Button";
+import Button from './Button';
 
 export default {
   title: 'Components/Button',
   component: Button,
-  tags: ['autodocs']
-}
+  tags: ['autodocs'],
+};
 
 export const Primary = {
   args: {
@@ -13,4 +13,10 @@ export const Primary = {
     transparent: false,
     icon: false,
   },
-}
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/PauMl6DJZIE2WwX8zcxble/AmaceIT---draft-1.0.0?type=design&node-id=1045-19429&mode=design&t=ze5a7pUMT7LSbAlT-11',
+    },
+  },
+};
