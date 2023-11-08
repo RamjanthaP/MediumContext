2ea28e4694634e28fa59d80ccdbc83639acde0d4
Joakim <joakim.nystrom@amaceit.se>
2023-11-08 15:42:09 +0100

    Adds story for Animated header

---
+++ src/components/AnimatedHeader/AnimatedHeader.stories.tsx
@@ -0,0 +1,18 @@
+import AnimatedHeader from './AnimatedHeader';
+
+export default {
+  title: 'Components/AnimatedHeader',
+  component: AnimatedHeader,
+  tags: ['autodocs'],
+};
+
+export const Example1 = {
+  args: {
+    title: 'Animated Header',
+  },
+};
+export const Example2 = {
+  args: {
+    title: 'Animated Header example 2',
+  },
+};
