3cc7e5ed645691c89090d82f20b1d5e080eee5fd
Joakim <joakim.nystrom@amaceit.se>
2023-11-08 15:11:58 +0100

    Adds utility

--- src/utilities/helper.tsx
+++ src/utilities/helper.tsx
@@ -1,9 +1,23 @@
+import { ReactElement, RefObject } from 'react';
+
 export function extractLastWord(headline: string) {
-  const words = headline.split(" ");
+  const words = headline.split(' ');
   if (words.length <= 1) return { remaining: headline, lastWord: headline };
 
   const lastWord = words.pop();
-  const remaining = words.join(" ");
+  const remaining = words.join(' ');
 
   return { remaining, lastWord };
 }
+
+/**
+ * Scrolls to the ref target element with an offset
+ */
+export const scrollToTarget = (
+  target: RefObject<HTMLElement>,
+  offsetTop = 200
+) => {
+  const top =
+    target.current?.offsetTop && target.current?.offsetTop - offsetTop;
+  window.scrollTo({ top, behavior: 'smooth' });
+};
