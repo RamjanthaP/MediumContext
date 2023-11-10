91056b0817f47ab08fbb56e0c7345485020be44d
Joakim <joakim.nystrom@amaceit.se>
2023-11-10 16:26:27 +0100

    Changes path to ecosystem next

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [
     {
       name: 'amaceit-website',
-      script: './node_modules/.bin/next',
+      script: './node_modules/next/dist/bin/next',
       args: 'start -p ' + (process.env.PORT || 3000),
       watch: false,
       autorestart: true,
