aa58693a7d6dc6b954f1a85b8e21a6b9673e828f
Botond Kupan <botond.kupan@amaceit.se>
2023-11-13 15:38:29 +0000

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       script: './node_modules/next/dist/bin/next',
       args: 'start -p ' + (process.env.PORT || 3000),
       watch: false,
-      autorestart: true,
-    },
-  ],
+      autorestart: true
+    }
+  ]
 };
