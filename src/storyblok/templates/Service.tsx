98f47d13bd466e0be06f6359776ff747de3d86bf
Joakim <joakim.nystrom@amaceit.se>
2023-11-13 09:15:02 +0100

    Creates a scaffold of Service Template

--- src/storyblok/templates/Service.tsx
+++ src/storyblok/templates/Service.tsx
@@ -1,15 +1,45 @@
 import { storyblokEditable, StoryblokComponent } from '@storyblok/react/rsc';
+
+import DemoAnimation from '@/components/AnimatedHeader/Demo.svg';
 import { TemplateServiceStoryblok } from '../../../component-types-sb';
+import RichText from '../helpers/RichText';
+import { Container } from '@/components/Layout/Container';
+import AnimateHeader from '@/components/AnimatedHeader/AnimatedHeader';
+import Placholder from '../fallback-component/Fallback';
 
 // Used as entry point for all pages in Storyblok
-const TemplateService = ({ blok }: TemplateServiceStoryblok) => {
-  console.log(blok);
+const TemplateService = ({ blok, title }: TemplateServiceStoryblok) => {
   return (
-    <main {...storyblokEditable(blok)}>
-      {blok.body?.map((nestedBlok: { _uid: string }) => (
-        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-      ))}
-    </main>
+    <>
+      <AnimateHeader
+        svgAnimation={DemoAnimation}
+        title={title}
+        topActionButton={{ text: 'Tjänster', url: '/services' }}
+      />
+
+      <main {...storyblokEditable(blok)}>
+        <Container className='mt-4 md:mt-8'>
+          <div className='grid md:grid-cols-12 gap-8'>
+            {blok.content && (
+              <div className='col-span-12 md:col-span-6'>
+                <RichText
+                  className='text-md md:text-lg'
+                  __html={blok.content}
+                />
+              </div>
+            )}
+            <div className='col-span-12 md:col-span-4 md:col-start-9'>
+              <Placholder blok={blok.quick_contact[0]} />
+            </div>
+          </div>
+        </Container>
+
+        <div className='col-4'></div>
+        {blok.body?.map((nestedBlok: { _uid: string }) => (
+          <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+        ))}
+      </main>
+    </>
   );
 };
 export default TemplateService;
