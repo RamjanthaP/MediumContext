b6e342dd958aba2dc964611bed05874f8e0937d9
Botond Kupan <botond.kupan@amaceit.se>
2023-11-13 15:46:10 +0000

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   apps: [
     {
-      name: 'amaceit-website',
+      name: 'amaceitwebstage01',
       script: './node_modules/next/dist/bin/next',
       args: 'start -p ' + (process.env.PORT || 3000),
       watch: false,
