a072b75daeccc50ffed6e44a6d3d8bbcd711d008
Joakim <joakim.nystrom@amaceit.se>
2023-11-15 15:16:14 +0100

    Adds som mapping utilities

---
+++ src/services/service-utilities.ts
@@ -0,0 +1,28 @@
+/**
+ * Replaces a property in an object with a new value
+ */
+export function replaceProperty<T, K extends keyof T>(
+  obj: T,
+  propertyKey: K,
+  newValue: T[K]
+): T {
+  return {
+    ...obj,
+    [propertyKey]: newValue,
+  };
+}
+/**
+ * Checks if a story has a relation for a specific property on content
+ */
+export function storyHasRelationForProp(
+  storyData: {
+    rels: { uuid: string }[];
+    story: { content: { [key: string]: unknown } };
+  },
+  key: string
+) {
+  return (
+    storyData.rels.length &&
+    storyData.rels.at(0)?.uuid === storyData.story.content[key]
+  );
+}
