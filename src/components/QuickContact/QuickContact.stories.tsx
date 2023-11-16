4c7916f4d4f7f735dda3724191012775a702b7e8
Tuliany <tuliany.grande@amaceit.se>
2023-11-16 15:43:56 +0100

    added data to quick component

--- src/components/QuickContact/QuickContact.stories.tsx
+++ src/components/QuickContact/QuickContact.stories.tsx
@@ -3,13 +3,11 @@ import QuickContact from "./QuickContact";
 export default {
   title: 'Components/QuickContact',
   component: QuickContact,
-  tags: ['autodics']
+  tags: ['autodocs']
 }
 
 export const Primary = {
   args: {
-    title: 'Tips & Råd',
-    description: 'Jag går gärna igenom processen ihop med er och hjälper er att formulera era behov!',
     person: {
       name: '',
       role: '',
--- src/components/QuickContact/QuickContact.tsx
+++ src/components/QuickContact/QuickContact.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 import Open from '../../../public/icons/open.svg'
 import Close from '../../../public/icons/close.svg'
-import SectionWrapper from '../Layout/SectionWrapper';
+import SectionWrapper from '../Layout/SectionWrapper'
 
-function QuickContact({ title, description, person }: any/* For now */) {
+function QuickContact({person }: any/* For now */) {
   const desktopBreakpoint = 1024;
   const [isDesktop, setIsDesktop] = useState(false);
   const [open, setOpen] = useState(false);
@@ -33,32 +33,31 @@ function QuickContact({ title, description, person }: any/* For now */) {
     }
   };
 
-  const containerClasses = `bg-discrete rounded-lg overflow-hidden ${open ? 'lg:w-max sm:w-full flex flex-col py-6 px-2 lg:px-16' : 'items-center w-max flex flex-row-reverse py-2 px-4'}`;
-  const toggleIconClasses = `lg:hidden cursor-pointer flex justify-end ${open ? 'mx-3' : 'ml-24'}`;
-  const contentClasses = `flex w-full py-2 ${open ? 'flex-col items-center' : 'items-center justify-between flex-row-reverse'}`;
+  const containerClasses = `bg-discrete rounded-lg overflow-hidden ${open ? 'lg:w-max sm:w-full flex flex-col py-6 px-2 lg:px-16' : 'items-center w-full flex flex-row-reverse py-2 px-4'}`;
+  const contentClasses = `flex w-full py-2 ${open ? 'flex-col items-center flex-col-reverse' : 'items-center justify-end flex-row-reverse'}`;
 
   return (
     <SectionWrapper>
       <div className={containerClasses}>
-        <button className={toggleIconClasses} onClick={toggleOpen} aria-expanded={open} aria-label="toggle-contact-info">
+        <button className="lg:hidden cursor-pointer flex justify-end" onClick={toggleOpen} aria-expanded={open} aria-label="toggle-contact-info">
           <Image src={open ? Close : Open} alt={open ? 'close-icon' : 'open-icon'} width="50" height="50" />
         </button>
         <div className={contentClasses}>
-          {title && <h2 className='text-xxl font-bold text-center'>{title}</h2>}
-          {person && <Image src={person.url} alt={person.alt} width={`${open ? "150" : "60"}`} height="150" className="rounded-full mx-auto" />}
+          <h2 className='text-xxl font-bold text-center px-2'>Tips & Råd</h2>
+          {person && <Image src={person.image} alt={person.name} width={`${open ? "150" : "60"}`} height="150" className="rounded-full" />}
         </div>
         {open && (
           <div className="flex flex-col items-center">
-            {description && <p className='text-xs mb-4 text-center'>{description}</p>}
+            <p className='text-xs mb-4 text-center'>Jag går gärna igenom processen ihop med er och hjälper er att formulera era behov</p>
             {person &&
               <div className='flex flex-col items-center my-2'>
                 <h5 className="text-xs font-bold">{person.name || 'Default Name'}</h5>
-                <p className='text-xs mb-3'>{person.role || 'Default Role'}</p>
+                <p className='text-xs mb-3'>{person.title || 'Default Role'}</p>
                 <div>
-                  <Button variant="primary">
+                  <Button variant="primary" href={`tel:${person.phone}`}>
                     Telefon
                   </Button>
-                  <Button variant="white" transparent>
+                  <Button variant="white" transparent href={`mailto:${person.email}`} >
                     Email
                   </Button>
                 </div>
