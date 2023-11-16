d1ed3b5b0a04fe0451f0077750c652989caba10f
Tuliany <tuliany.grande@amaceit.se>
2023-11-16 11:10:12 +0100

    git

--- src/components/QuickContact/QuickContact.stories.tsx
+++ src/components/QuickContact/QuickContact.stories.tsx
@@ -3,7 +3,7 @@ import QuickContact from "./QuickContact";
 export default {
   title: 'Components/QuickContact',
   component: QuickContact,
-  tags: ['autodocs']
+  tags: ['autodics']
 }
 
 export const Primary = {
