c10be7f551973dbce345c5ebc34fe2cb121565a9
Joakim <joakim.nystrom@amaceit.se>
2023-11-22 13:55:52 +0100

    fix(TitleElement): Adding key to iteration on dot

--- src/components/TitleElement/TitleElement.tsx
+++ src/components/TitleElement/TitleElement.tsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 type TitleElementProps = {
   title: string;
-  titleSize?: "h1" | "h2" | "h3" | "span";
-}
+  titleSize?: 'h1' | 'h2' | 'h3' | 'span';
+};
 
 function TitleElement({ title }: TitleElementProps) {
   function colorDots(title: string) {
     const parts = title.split('.').map((part, index, array) =>
-      index !== array.length - 1 ? <><span>{part}</span><span className='text-primary-500'>.</span></> : <span>{part}</span>
+      index !== array.length - 1 ? (
+        <>
+          <span key={index}>{part}</span>
+          <span className='text-primary-500' key={index}>
+            .
+          </span>
+        </>
+      ) : (
+        <span key={index}>{part}</span>
+      )
     );
 
     return <>{parts}</>;
   }
   return (
     <div>
-      <h1 className='text-xl md:text-3xl font-bold mb-4'>
-        {colorDots(title)}
-      </h1>
+      <h1 className='text-xl md:text-3xl font-bold mb-4'>{colorDots(title)}</h1>
     </div>
-  )
+  );
 }
 
-export default TitleElement
+export default TitleElement;
