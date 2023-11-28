01c1a15858a25da5d8f7d2209e28418a08d437cb
Joakim <joakim.nystrom@amaceit.se>
2023-11-28 23:16:15 +0100

    Adds stories

---
+++ src/components/Feature/Feature.stories.tsx
@@ -0,0 +1,31 @@
+import Feature, { FeatureSectionProps } from './Feature';
+
+export default {
+  title: 'Components/Feature',
+  component: Feature,
+  tags: ['autodocs'],
+};
+
+const defaultArgs: FeatureSectionProps = {
+  bgColor: 'default',
+  firstButton: {
+    text: 'Primary',
+    url: '#',
+  },
+  secondButton: {
+    text: 'Secondary',
+    url: '#',
+  },
+  image: {
+    url: 'https://via.placeholder.com/500',
+    alt: 'Placeholder',
+  },
+  preTitle: 'Pretitle.',
+  title: 'Title',
+  body: 'Here is some body text.',
+};
+
+export const BasicWithLinks = {
+  args: defaultArgs,
+};
+
