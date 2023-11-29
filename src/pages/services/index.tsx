970704f1dcf127fdd11db9bbd7dd728450f80290
Joakim <joakim.nystrom@amaceit.se>
2023-11-29 10:37:01 +0100

    WIP: Service in page router

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,27 +1,35 @@
 import StoryblokStory from '@storyblok/react/story';
-import { getStoryblokPage, getRelatedItems } from '@/services/getStoryBlokPage';
+import {
+  getStoryblokPage,
+  getGlobalServiceItems,
+} from '@/services/getStoryBlokPage';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { StoryblokComponent } from '@storyblok/react';
+import AnimateHeader from '@/components/AnimatedHeader/AnimatedHeader';
+import DemoAnimation from '@/components/AnimatedHeader/animations/Demo.svg';
 
 // Return a list of `params` to populate the [slug] dynamic segment
 // TODO: Make a fetch to the Storyblok API to get all the slugs we can forsee
 
-export default function Page(props: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Page(
+  props: InferGetStaticPropsType<typeof getStaticProps>
+) {
   return (
     <div>
-      <StoryblokStory
-        story={props.pageData.props.story}
-      />
+      <AnimateHeader title='Tjänster' svgAnimation={DemoAnimation} />
+      <StoryblokComponent blok={props.relatedItemRequest.content} />
+      <StoryblokStory story={props.pageData.props.story} />
     </div>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const pageData = await getStoryblokPage();
-  const relatedItemRequest = await getRelatedItems();
+  const pageData = await getStoryblokPage(['services']);
+  const relatedItemRequest = await getGlobalServiceItems();
   return {
     props: {
       pageData,
-      relatedItemRequest
-    }
-  }
-}
+      relatedItemRequest,
+    },
+  };
+};
