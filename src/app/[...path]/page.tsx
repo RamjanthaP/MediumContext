31cd2be3a05e589987cb6f14285c72254e72b2ee
Joakim <joakim.nystrom@amaceit.se>
2023-11-29 10:35:49 +0100

    Removes request for related items on app router path

--- src/app/[...path]/page.tsx
+++ src/app/[...path]/page.tsx
@@ -1,5 +1,5 @@
 import StoryblokStory from '@storyblok/react/story';
-import { getStoryblokPage, getRelatedItems } from '../../services/getStoryBlokPage';
+import { getStoryblokPage } from '../../services/getStoryBlokPage';
 
 // Return a list of `params` to populate the [slug] dynamic segment
 // TODO: Make a fetch to the Storyblok API to get all the slugs we can forsee
@@ -19,15 +19,14 @@ export async function generateStaticParams() {
 export default async function Page({ params }: { params: { path: string[] } }) {
   const path = params.path;
   const { props } = await getStoryblokPage(path);
-  const relatedItemRequest = await getRelatedItems()
 
   return (
     <div>
+      <h1 className='text-3xl'>Korven</h1>
       <StoryblokStory
         story={props.story}
         contactPerson={props.story.contact_person}
         title={props.story.name}
-        relatedItem={relatedItemRequest}
       />
     </div>
   );
