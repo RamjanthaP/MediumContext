6fd21a20fbf726491ed20c8436ab2d89428284ec
Tuliany <tuliany.grande@amaceit.se>
2023-11-30 11:26:05 +0100

    helper function for url path and slug

--- src/utilities/helper.tsx
+++ src/utilities/helper.tsx
@@ -1,3 +1,5 @@
+import { getStaticProps } from '@/pages/services';
+import { InferGetStaticPropsType } from 'next';
 import { RefObject } from 'react';
 
 export function extractLastWord(headline: string) {
@@ -31,4 +33,24 @@ export function getSlugParam(path: string | string[] | undefined) {
   if (!path) return '';
   if (typeof path === 'string') return path;
   return path.join('/');
+}
+export const processPath = (path: string | string[] | undefined): string => {
+  if (!path) return '';
+  if (typeof path === 'string') return path.toLowerCase();
+  return Array.isArray(path) && path.length > 0 ? path[0].toLowerCase() : '';
+};
+
+export const filterRelatedItems = (path: string | string[] | undefined, relatedItemRequest: InferGetStaticPropsType<typeof getStaticProps>) => {
+  let url = processPath(path)
+  const filteredColumns = relatedItemRequest.content.columns.filter((column: { title: string; }) =>
+    column.title.toLowerCase() !== url.toLowerCase()
+  );
+
+  return {
+    ...relatedItemRequest,
+    content: {
+      ...relatedItemRequest.content,
+      columns: filteredColumns
+    }
+  };
 }
