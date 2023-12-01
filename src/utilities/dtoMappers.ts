11f7aa186267f520cdff07eec83d1a28775b3de9
Tuliany <tuliany.grande@amaceit.se>
2023-12-01 20:57:15 +0100

    updated helper function in hero

--- src/utilities/dtoMappers.ts
+++ src/utilities/dtoMappers.ts
@@ -3,10 +3,12 @@ import { LinkStoryblok } from "@sb-types";
 export function extractLinkDataFromFirstItem(
   link: LinkStoryblok[] | undefined
 ) {
-  return link?.length
-    ? {
-        text: link[0].text,
-        url: link[0].link.cached_url,
-      }
-    : undefined;
+  if (link && link.length > 0) {
+    const url = link[0].link?.cached_url || ''; 
+    return {
+      text: link[0].text,
+      url: url,
+    };
+  }
+  return undefined;
 }
