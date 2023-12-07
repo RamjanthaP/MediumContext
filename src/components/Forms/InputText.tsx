7aac171945c89f2e75708aaa75e9bcfe049ef0a1
Joakim <joakim.nystrom@amaceit.se>
2023-12-07 15:39:38 +0100

    Moves tailwind classes to css module

--- src/components/Forms/InputText.tsx
+++ src/components/Forms/InputText.tsx
@@ -1,11 +1,18 @@
 import { BaseProps } from '@/types/props';
 
+import Style from './input-text.module.css';
+
 export interface InputTextProps extends BaseProps {
   id: string;
   value: string;
   error?: string;
-  onChange: (value: string) => void;
+  onChange: (newValue: string) => void;
 }
+export interface InputTextProps
+  extends Omit<
+    Partial<React.InputHTMLAttributes<HTMLInputElement>>,
+    'onChange' | 'value' // Ommitting this in order to make it required for the component
+  > {}
 
 const InputText = ({
   children,
@@ -14,36 +21,26 @@ const InputText = ({
   value,
   error,
   onChange,
+  ...inputProps
 }: InputTextProps) => {
   const borderColor = error
     ? 'border-error'
     : 'border-transparent focus:border-primary-500';
+
   return (
     <div className={`flex flex-col ${className}`}>
-      <label className='my-1 mx-6 text-md font-semibold' htmlFor={id}>
+      <label className={Style.label} htmlFor={id}>
         {children}
       </label>
       <input
-        className={[
-          'bg-default',
-          'border-2',
-          'focus:outline-none',
-          'py-2',
-          'px-6',
-          'rounded-full',
-          'font-bold',
-          borderColor,
-        ].join(' ')}
+        {...inputProps}
+        className={`bg-default ${Style.field} ${borderColor}`}
         type='text'
         id={id}
         value={value}
         onChange={(e) => onChange(e.target.value)}
       />
-      {error && (
-        <div className='text-error text-sm my-1 mx-6 font-semibold'>
-          {error}
-        </div>
-      )}
+      {error && <div className={Style.errorMessage}>{error}</div>}
     </div>
   );
 };
