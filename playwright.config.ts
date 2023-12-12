f42e73752a9a25317e4c085b5ac19dcd88000452
Joakim <joakim.nystrom@amaceit.se>
2023-12-12 06:11:44 +0100

    Updates config

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     },
     {
       name: 'firefox',
-      use: { ...devices['iPhone 14 Pro'] },
+      use: { ...devices['Desktop Firefox'] },
     },
   ],
   // Run your local dev server before starting the tests.
