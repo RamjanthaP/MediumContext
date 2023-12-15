5f73b605dcd93ea127e1d21c3f4ebf985bae413f
Joakim <joakim.nystrom@amaceit.se>
2023-12-15 12:15:32 +0100

    Allow console.log for debugging

--- src/utilities/dev-utils.tsx
+++ src/utilities/dev-utils.tsx
@@ -1,4 +1,5 @@
 export function colorConsole(message: string, color: string = 'orange') {
+  // eslint-disable-next-line no-console
   console.log(
     `%c${message}`,
     `background: ${color}; padding: 5px 10px; border-radius: 5px;`
