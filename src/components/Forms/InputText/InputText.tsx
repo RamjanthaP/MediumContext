5ccf3a0bfaa734f2ec1700b7c8233cb895c1e414
Tuliany <tuliany.grande@amaceit.se>
2023-12-19 11:59:53 +0100

    not working with both textarea and inputarea

--- src/components/Forms/InputText/InputText.tsx
+++ src/components/Forms/InputText/InputText.tsx
@@ -5,18 +5,16 @@ import React from 'react';
 
 export interface InputTextProps extends BaseProps {
   id: string;
-  //value: string;
+  value?: string;
   error?: string;
-  //onChange: (_newValue: string) => void;
 }
 
 const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(
-  ({ children, className, id, value, error, onChange, ...inputProps }, ref) => {
+  ({ children, className, id, value, error, ...inputProps }, ref) => {
     InputText.displayName = 'InputText';
     const borderColor = error
       ? 'border-error'
       : 'border-inverted focus:border-primary-500';
-
     return (
       <div className={`flex flex-col ${className}`}>
         <label className={Style.label} htmlFor={id}>
@@ -24,15 +22,13 @@ const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(
         </label>
         <input
           {...inputProps}
-          ref={ref} // Attach the forwarded ref to the input element
+          ref={ref}
           className={`bg-default ${Style.field} ${borderColor}`}
           type='text'
           id={id}
-          //value={value}
-         //onChange={(e) => onChange(e.target.value)}
+          value={value}
         />
-{/*         {error && <div className={Style.errorMessage}>{error}</div>}
- */}      </div>
+      </div>
     );
   }
 );
--- src/storyblok/components/FormBlock.tsx
+++ src/storyblok/components/FormBlock.tsx
@@ -1,31 +1,51 @@
 import Button from "@/components/Button/Button";
 import { FormInputsStoryblok, FormStoryblok } from "@sb-types";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 export default function Form({ blok }: FormStoryblok) {
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const {
     register,
+    reset,
     formState: { errors },
     handleSubmit,
+    watch
   } = useForm();
+  const invisibleRadioButton = watch("invisibleRadioButton", false);
 
-  function submitForm(data :any) {
+  function submitForm(data: any) {
+    if (invisibleRadioButton) {
+      console.log("not today satan");
+      return; 
+    }
     console.log(data, blok.Endpoint);
+    reset();
+    setIsSubmitted(true);
   }
   return (
     <div className="container mx-auto w-1/2">
-
-   
-    <form {...storyblokEditable(blok)} onSubmit={handleSubmit(submitForm)}>
+      <form {...storyblokEditable(blok)} onSubmit={handleSubmit(submitForm)}>
         {blok.Inputs.map((nestedBlok: FormInputsStoryblok) => (
-        <StoryblokComponent
-          blok={nestedBlok}
-          key={nestedBlok._uid}
-          register={register}
-          errors={errors}
+          <StoryblokComponent
+            blok={nestedBlok}
+            key={nestedBlok._uid}
+            register={register}
+            errors={errors}
+          />
+        ))}
+        <input
+          type="radio"
+          {...register("invisibleRadioButton")}
+          value="checked"
+          className="inline-grid border-transparent"
         />
-      ))}
-      <Button variant="primary" type="submit"> Sänd </Button>
+        <div className="my-2">
+        <Button variant="primary" type="submit">
+          {isSubmitted ? 'Skickat' : 'Sänd'}
+          </Button>
+        </div>
+        <p>{isSubmitted && 'Vi har tagit emot ditt meddelande'}</p>
       </form>
     </div>
   );
