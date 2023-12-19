8b4fc9772ccbd16cd544ca7e20af4fc7745a79e5
Tuliany <tuliany.grande@amaceit.se>
2023-12-19 21:26:17 +0100

    responsive design

--- src/components/TestimonialCard/TestimonialCard.tsx
+++ src/components/TestimonialCard/TestimonialCard.tsx
@@ -13,11 +13,11 @@ function TestimonialCard({  image, name, title, quote }: TestimonialCardProps) {
   return (
     <div className='bg-discrete'>
       <div className="container mx-auto">
-        <div className="md:flex p-4 justify-center h-4/6">
-          <div className="w-1/3 h-48 md:h-auto md:w-48 items-center my-auto lg:mr-12">
+        <div className="md:flex p-4 justify-center md:flex-col md:items-center lg:flex-row">
+          <div className="h-48 md:h-auto md:w-48 items-center my-auto lg:mr-12 mx-auto mb-8">
             <Image className="rounded-full" src={`https:${image}`} alt={name ? name : 'Testimonial Image'} height="250" width="250"/>
           </div>
-          <div className="w-2/3 p-8 bg-default rounded-xl">            
+          <div className="lg:w-2/3 p-8 bg-default rounded-xl">            
             <p className="mt-2 text-xl p-1">{`"${quote}"`}</p>
             <div className='flex items-center mt-4'>
               <p className="text-lg text-bold">{name}</p>,
