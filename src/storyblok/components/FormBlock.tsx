243415de5a4805fbbd3b8802c5be597331ff8bcb
Joakim <joakim.nystrom@amaceit.se>
2023-12-19 15:48:06 +0100

    Adding some margin

--- src/storyblok/components/FormBlock.tsx
+++ src/storyblok/components/FormBlock.tsx
@@ -1,8 +1,10 @@
-import Button from "@/components/Button/Button";
-import { FormInputsStoryblok, FormStoryblok } from "@sb-types";
-import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useState } from 'react';
+
+import { FormInputsStoryblok, FormStoryblok } from '@sb-types';
+import { StoryblokComponent, storyblokEditable } from '@storyblok/react';
+import { useForm } from 'react-hook-form';
+
+import Button from '@/components/Button/Button';
 
 export default function Form({ blok }: FormStoryblok) {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -11,22 +13,26 @@ export default function Form({ blok }: FormStoryblok) {
     reset,
     formState: { errors },
     handleSubmit,
-    watch
+    watch,
   } = useForm();
-  const invisibleRadioButton = watch("invisibleRadioButton", false);
+  const invisibleRadioButton = watch('invisibleRadioButton', false);
 
   function submitForm(data: any) {
     if (invisibleRadioButton) {
-      console.log("not today satan");
-      return; 
+      console.log('not today satan');
+      return;
     }
     console.log(data, blok.Endpoint);
     reset();
     setIsSubmitted(true);
   }
   return (
-    <div className=" mx-auto py-4 bg-discrete">
-      <form {...storyblokEditable(blok)} onSubmit={handleSubmit(submitForm)} className="lg:w-1/3 mx-auto my-2">
+    <div className='mx-auto py-4 bg-discrete'>
+      <form
+        {...storyblokEditable(blok)}
+        onSubmit={handleSubmit(submitForm)}
+        className='mx-4 md:mx-8 lg:w-1/3 lg:mx-auto  my-2'
+      >
         {blok.Inputs.map((nestedBlok: FormInputsStoryblok) => (
           <StoryblokComponent
             blok={nestedBlok}
@@ -36,14 +42,14 @@ export default function Form({ blok }: FormStoryblok) {
           />
         ))}
         <input
-          type="radio"
-          {...register("invisibleRadioButton")}
-          value="checked"
-          className="inline-grid border-transparent"
+          type='radio'
+          {...register('invisibleRadioButton')}
+          value='checked'
+          className='inline-grid border-transparent'
         />
-        <div className="mt-4 flex justify-end">
-        <Button variant="primary" type="submit">
-          {isSubmitted ? 'Skickat' : 'Skicka'}
+        <div className='mt-4 flex justify-end'>
+          <Button variant='primary' type='submit'>
+            {isSubmitted ? 'Skickat' : 'Skicka'}
           </Button>
         </div>
         <p>{isSubmitted && 'Vi har tagit emot ditt meddelande'}</p>
