3fda602e777701322c56fb11bd56a10352f8856c
Tuliany <tuliany.grande@amaceit.se>
2023-12-20 22:11:03 +0100

    change css location import

--- src/storyblok/components/Carousel.tsx
+++ src/storyblok/components/Carousel.tsx
@@ -3,6 +3,8 @@ import TestimonialCard from '@/components/TestimonialCard/TestimonialCard';
 import { storyblokEditable } from '@storyblok/react';
 import React from 'react'
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface CarouselSbProps {
   _uid: string,
