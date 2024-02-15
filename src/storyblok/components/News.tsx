3c232d9b42a8d72b18d77df6b3f0bab9ce5926ce
Tuliany <tuliany.grande@amaceit.se>
2024-02-15 13:16:03 +0100

    added new card component with allowing external link

--- src/storyblok/components/News.tsx
+++ src/storyblok/components/News.tsx
@@ -1,36 +1,45 @@
-import { AssetStoryblok, FeatureStoryblok } from '@sb-types';
+import { FeatureStoryblok } from '@sb-types';
 import { storyblokEditable } from '@storyblok/react/rsc';
-
-import FeatureExpandable from '@/components/Feature/FeatureExpandable';
-
+import NewsCard from '@/components/NewsCard/NewsCard';
 
 const NewsSb = ({ blok }: FeatureStoryblok) => {
-    const componentData = mapNewsToData(blok);
+  const componentData = mapNewsToData(blok);
+
   return (
-      <div {...storyblokEditable(blok)}>
-      <FeatureExpandable {...componentData} />
+    <div {...storyblokEditable(blok)}>
+      {componentData.map((item) => (
+        <NewsCard key={item.uid} {...item} />
+      ))}
       </div>
     );
   
 };
 export default NewsSb;
 
-const mapNewsToData = (
-  blok: any
-) => {
-  const image: AssetStoryblok | undefined = blok.image;
-  return {
-    title: blok.title,
-    image: image
-      ? {
-        alt: image.alt || '', 
+const mapNewsToData = (blok :any) => {
+  const { block } = blok;
+
+  if (!Array.isArray(block)) {
+    console.error('Expected block to be an array');
+    return [];
+  }
+
+  // Map each item in the block to a new structure
+  return block.map(item => {
+    const { link, image, title, caption } = item;
+    console.log(link)
+
+    const mappedItem = {
+      uid: item._uid,
+      title: title,
+      caption: caption,
+      link: link, 
+      image: image && {
+        alt: image.alt || '',
         url: image.filename,
-      }
-      : undefined,
-    body: blok.caption,
-    exBody: {
-      expandText: blok.link && blok.link.text || blok?.link.cached_url,
-    }
-    
-  };
+      },
+    };
+
+    return mappedItem;
+  });
 };
