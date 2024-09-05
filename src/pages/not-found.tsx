692caaa746ad17a673803314fc2a78775bed35e9
Joakim Nyström <joakim.nystrom@amaceit.se>
2024-09-05 19:23:46 +0000

    Merged PR 1055: Adjustments of 404 illustration
    ## Adds
    
    -
    
    ## Changes
    
    - classes on 404-page illustration in order to better sizing
    
    ## Removes
    
    -
    
    ## Screenshots
    
    ### Mobile
    
    ### Desktop

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,19 +1,26 @@
+import Image from 'next/image';
+
 import { ArrowLeftIcon } from '@heroicons/react/20/solid';
 
 import Button from '@/components/Button/Button';
 import { Container } from '@/components/Layout/Container';
-import Image from 'next/image';
+
 import notFound from '../../public/assets/404.svg';
+
 export default function NotFound() {
   return (
     <div className='min-h-[50vh]'>
       <Container>
         <div className='flex justify-center'>
-          <div className='w-1/2 flex flex-col gap-4 pt-12'>
+          <div className='w-3/4 lg:w-1/2 flex flex-col gap-4 pt-12'>
             <h2 className='text-xxl mx-auto'>Sidan kunde inte hittas!</h2>
-            <Image src={notFound} alt="not-found"
-              width={600} height={500}
-              className='object-contain' />
+            <Image
+              src={notFound}
+              alt='not-found'
+              width={600}
+              height={500}
+              className='object-contain'
+            />
             <p className='text-lg'>
               Den sidan som du letar efter är antingen borttagen eller flyttad
               (och vi har slarvat med redirecten. Ber om ursäkt i så fall. 😅).
