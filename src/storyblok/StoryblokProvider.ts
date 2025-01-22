7fe3ff1d3ca55d4508cb30b9655fe21f3352240c
Abrei852 <abrahameishow95@gmail.com>
2025-01-22 10:05:10 +0100

    merged incoming data type with component

--- src/storyblok/StoryblokProvider.ts
+++ src/storyblok/StoryblokProvider.ts
@@ -5,6 +5,8 @@ import FallbackComponent from '@/storyblok/fallback-component/Fallback';
 /** Import your components */
 import TemplateDefault from '@/storyblok/templates/Default';
 import TemplateService from '@/storyblok/templates/Service';
+import TemplateStart from '@/storyblok/templates/Start';
+
 import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
 
 import HeaderMenu from '@/components/Menu/HeaderMenu';
@@ -17,6 +19,7 @@ import Feature from './components/Feature';
 import FormField from './components/Forms/FormField';
 import GridSb from './components/Grid';
 import HeroSb from './components/Hero';
+import HeroStartSb from './components/HeroStart';
 import JumbotronSb from './components/Jumbotron';
 import LogoCardSb from './components/LogoCard';
 import MapSb from './components/MapSb';
@@ -29,6 +32,7 @@ import ServiceItemSb from './components/ServiceItems';
 
 const components = {
   template_default: TemplateDefault,
+  template_start: TemplateStart,
   template_service: TemplateService,
   template_news: NewsSb,
   body: BodyBlockSb,
@@ -36,6 +40,7 @@ const components = {
   grid: GridSb,
   map_block: MapSb,
   hero: HeroSb,
+  hero_start: HeroStartSb,
   header_menu: HeaderMenu,
   menu_link: MenuLink,
   logo_card: LogoCardSb,
