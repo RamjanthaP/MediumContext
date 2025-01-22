34c5cbe9e7c6a897dad165e1bde9b07778b4ed94
Abrei852 <abrahameishow95@gmail.com>
2025-01-22 11:31:57 +0100

    template for template start

---
+++ src/storyblok/templates/Start.tsx
@@ -0,0 +1,17 @@
+import { TemplateStartStoryblok } from '@sb-types';
+import { StoryblokComponent, storyblokEditable } from '@storyblok/react/rsc';
+
+// Used as entry point for all pages in Storyblok
+const TemplateStart = ({ blok }: TemplateDefaultStoryblok) => {
+  return (
+    <main {...storyblokEditable(blok)}>
+      {blok.StartHero?.map((nestedBlok: { _uid: string }) => (
+        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+      ))}
+      {blok.body?.map((nestedBlok: { _uid: string }) => (
+        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+      ))}
+    </main>
+  );
+};
+export default TemplateStart;
